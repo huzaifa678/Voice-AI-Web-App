3b9145faa0eb42b44d427b3f7fc551fd65e06667
huzaifa678 <huzaifaahmad2210@gmail.com>
2026-08-06 16:29:10 +0500

    Use config for redis url

--- voiceAI/app/common/redis.py
+++ voiceAI/app/common/redis.py
@@ -1,9 +1,5 @@
-import os
-from dotenv import load_dotenv
 import redis
 
-load_dotenv()
+from app.common.config import config
 
-REDIS_URL = os.getenv("REDIS_URL") or os.getenv("CELERY_RESULT_BACKEND")
-
-redis_client = redis.Redis.from_url(REDIS_URL, decode_responses=True)
+redis_client = redis.Redis.from_url(config.REDIS_URL, decode_responses=True)
